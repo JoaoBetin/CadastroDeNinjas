aa0b84641952b3daa1fb4ec9023eaf1ca9b01ed1
Joao Betin <betinjoao2@gmail.com>
2026-01-26 22:31:54 -0300

    feat: Cria as rotas com o mock do MissaoController

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,7 +3,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninja")
 public class NinjaController {
 
     // Adicionar ninja
